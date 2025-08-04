1b323cfb516f21b4330ced231d264e1d45550776
Ali Javed <jali084337@gmail.com>
2025-08-04 18:24:00 +0500

    create sponsered section

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets';
+import { assets, dummyPostsData } from '../assets/assets';
 import Loading from '../components/Loading';
 import StoriesBar from '../components/StoriesBar';
 import PostCard from '../components/PostCard';
@@ -35,9 +35,14 @@ function Feed() {
       </div>
 
       {/* Right sidebar */}
-      <div>
-        <div className="">
-          <h1>Sponsered</h1>
+      <div className='max-xl:hidden sticky top-2'>
+        <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
+          <h3>Sponsered</h3>
+
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+          <p className='text-slate-600'>Email Markiting</p>
+          <p className='text-slate-400'>Supercharge your marketing with a powerful, easy to use platform built for results</p>
+
         </div>
         <h1>Recent Messages</h1>
       </div>
